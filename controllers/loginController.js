799fe24a577f88255f17f4dbcc9fc176b0a0c489
Tobias Dodge <tobydodge65@gmail.com>
2024-05-31 13:33:17 -0230

    tokenGenerator added

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -17,13 +17,17 @@ const tokenGenerator = async (req, res) => {
         const { Username, PasswordHash } = req.body
         const user = await User.findOne({Username: Username, PasswordHash: PasswordHash})
         const token = await bcrypt.hash(user._id.toString() + "_" + Date.now, 10)
-        const dategen = Date.now().toString()
+        const dategen = Date.now()
+        const validUntil = Math.round(dategen + (10 * 60000))  // 10 minutes
 
         const returnData = {
             token: token,
-            date: dategen
+            date: dategen,
+            validUntil: validUntil
         }
 
+        const updatedUser = await User.findOneAndUpdate({Token: token})
+        
         return res.status(200).json({ data: returnData })
     } catch (error) {
         return res.status(500).json({ error: error.message })
