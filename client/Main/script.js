1cba7ef31c9e04d4e9e8a04e6567a1d72bd6a76b
Tobias Dodge <tobydodge65@gmail.com>
2024-06-04 18:25:01 -0230

    Temp place for account input

--- client/Main/script.js
+++ client/Main/script.js
@@ -1,5 +1,6 @@
 const passwordArea = document.querySelector("#passwordArea")
 const logoutButton = document.querySelector("#logout")
+const addPasswordButton = document.querySelector("#addPassword")
 
 const TOKEN = localStorage.getItem('token')
 const endpoint = "http://127.0.0.1:3000/accounts"
@@ -31,6 +32,23 @@ async function listAccounts() {
             </div>
         `
     })
+}
+
+async function addAccount() {
+    let websiteName = document.querySelector("#websiteName").value
+    let websiteURL = document.querySelector("#websiteURL").value
+    let username = document.querySelector("#username").value
+    let password = document.querySelector("#password").value
+
+    let res = await axios.post(endpoint, {
+        Token: TOKEN,
+        WebsiteName: websiteName,
+        WebsiteURL: websiteURL,
+        Username: username,
+        Password: password
+    })
+
+    console.log(res)
 }
 
 function logout() {
