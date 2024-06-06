75e682acefde14dedfc3df41888fc06896e0cdf8
Tobias Dodge <tobydodge65@gmail.com>
2024-06-06 16:05:56 -0230

    Updating passwords now works

--- client/Main/script.js
+++ client/Main/script.js
@@ -11,8 +11,6 @@ const userEndpoint = "http://192.168.4.123:3000/users"
 
 console.log(TOKEN)
 
-let passwords = {}
-
 async function listAccounts() {
     let res = await axios.get(endpoint, {
         headers: {
@@ -26,8 +24,10 @@ async function listAccounts() {
 
     console.log(res)
 
+    let tempContent = ''
+
     res.data.accounts.forEach(account => {
-        passwordArea.innerHTML += `
+        tempContent += `
             <div id="passwordInfo">
                 <h2>${account.WebsiteName}</h2>
                 <a href="${account.WebsiteURL}">URL</a>
@@ -38,33 +38,112 @@ async function listAccounts() {
                 <button id="editButton_${account._id}">Edit</button>
             </div>
         `
-
-        passwords[account.WebsiteName] = account._id
     })
 
-    // TODO: Move the for loop up into the forEach loop
 
-    for(let key in passwords) {
-        if(passwords.hasOwnProperty(key)) {
-            console.log(passwords[key])
+    passwordArea.innerHTML = tempContent
 
-            const delBtn = document.querySelector(`#deleteButton_${passwords[key]}`)
-            const editBtn = document.querySelector(`#editButton_${passwords[key]}`)
+    res.data.accounts.forEach(account => {
+        const delBtn = document.getElementById(`deleteButton_${account._id}`)
+        const editBtn = document.getElementById(`editButton_${account._id}`)
 
-            delBtn.addEventListener("click", async function() {
-                alert(`Hello from deleteButton_${passwords[key]}`)
+        delBtn.addEventListener("click", async function() {
+
+            const overlay = document.createElement('div')
+            overlay.id = 'overlay'
+
+            const card = document.createElement('div')
+            card.id = 'card'
+            card.className = 'dialog'
+            card.innerHTML = `
+                <h1>Are you sure you want to delete this account?</h1>
+                <button id="yes">Yes</button>
+                <button id="no">No</button>
+            `
+
+            document.body.appendChild(overlay)
+            document.body.appendChild(card)
+
+            const yes = document.querySelector("#yes")
+            const no = document.querySelector("#no")
+
+            yes.addEventListener("click", async function() {
                 deletePassword(passwords[key])
+                document.body.removeChild(card)
+                document.body.removeChild(overlay)
             })
 
-            editBtn.addEventListener("click", async function() {
-                alert(`Hello from editButton_${passwords[key]}`)
+            no.addEventListener("click", async function() {
+                document.body.removeChild(card)
+                document.body.removeChild(overlay)
+            })
+        })
+
+        editBtn.addEventListener("click", async function() {
+            const overlay = document.createElement('div')
+            overlay.id = 'overlay'
+            const card = document.createElement('div')
+            card.id = 'card'
+            card.className = 'dialog'
+            card.innerHTML = `
+                <input id="websiteName_${account._id}" type="text" placeholder="Website Name" autocomplete="off" value="${account.WebsiteName}">
+                <input id="websiteURL_${account._id}" type="text" placeholder="Website URL" autocomplete="off" value="${account.WebsiteURL}">
+                <input id="username_${account._id}" type="text" placeholder="Website Username" autocomplete="off" value="${account.Username}">
+                <input id="password_${account._id}" type="password" placeholder="Website Password" autocomplete="off" value="${account.Password}">
+                <button id="submit_${account._id}" class="password-card-button">Submit</button>
+                <button id="cancel_${account._id}" class="password-card-button">Cancel</button>
+            `
+            document.body.appendChild(overlay)
+            document.body.appendChild(card)
+            
+            const submit = document.querySelector(`#submit_${account._id}`)
+            const cancel = document.querySelector(`#cancel_${account._id}`)
+
+            submit.addEventListener("click", async function() {
+                const newName = document.querySelector(`#websiteName_${account._id}`).value
+                const newURL = document.querySelector(`#websiteURL_${account._id}`).value
+                const newUser = document.querySelector(`#username_${account._id}`).value
+                const newPass = document.querySelector(`#password_${account._id}`).value
+
+                await updateAccount(account._id, newName, newURL, newUser, newPass)
 
                 passwordArea.innerHTML = ""
 
                 await listAccounts()
+
+                document.body.removeChild(card)
+                document.body.removeChild(overlay)
             })
+
+            cancel.addEventListener("click", async function() {
+                document.body.removeChild(card)
+                document.body.removeChild(overlay)
+            })
+        })
+    })
+}
+
+async function updateAccount(_id, webName, webURL, user, pass) {
+    console.log("Updating account with:", {
+        _id,
+        webName,
+        webURL,
+        user,
+        pass
+    })
+
+    const res = await axios.put(endpoint, {
+        Token: TOKEN,
+        _id: _id,
+        WebsiteName: webName,
+        WebsiteURL: webURL,
+        Username: user,
+        Password: pass
+    }).catch(function(error) {
+        if(error.response.status == 401) {
+            alert("Invalid or expired token")
         }
-    }
+    })
 }
 
 async function addAccount() {
